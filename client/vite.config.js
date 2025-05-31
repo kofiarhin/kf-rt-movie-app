9aa968f36390845d4f25d17eb2ec36396df92ccf
kofi arhin <kofiarhin69@gmail.com>
2025-06-01 00:37:02 +0100

    made changes to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://kf-rt-movie-app.onrender.com",
         changeOrigin: true,
       },
     },
