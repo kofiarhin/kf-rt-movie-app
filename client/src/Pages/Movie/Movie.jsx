0232fddf3d26b4bc1d024e95dbe6abda91a9697e
kofi arhin <kofiarhin69@gmail.com>
2025-06-02 12:09:29 +0100

    update code clean up trailer

--- client/src/Pages/Movie/Movie.jsx
+++ client/src/Pages/Movie/Movie.jsx
@@ -7,13 +7,21 @@ import useCredits from "../../hooks/useCredits";
 import Cast from "../../components/Cast/Cast";
 import LazyImage from "../../components/Lazy/LazyImage";
 import useTrailer from "../../hooks/useTrailer";
+import { Link } from "react-router-dom";
 
 const Movie = () => {
   const { id } = useParams();
-  const { data: creditsData } = useCredits(id);
+  // const { data: creditsData } = useCredits(id);
   const { data, isLoading, error } = useMovie(id);
-  const { data: trailerData } = useTrailer(id);
-  console.log(trailerData);
+  // const { data: trailerData } = useTrailer(id);
+
+  let movieData, trailerData, castData, trailerKey;
+  if (data) {
+    movieData = data[0];
+    castData = data[1].cast;
+    trailerData = data[2].results[0];
+    trailerKey = trailerData.key;
+  }
 
   if (error)
     return (
@@ -25,33 +33,35 @@ const Movie = () => {
   return (
     <div id="movie">
       <div className="content-wrapper">
-        <Image url={data.poster_path} />
+        <Image url={movieData.poster_path} />
         <div className="details-wrapper">
-          <h1 className="heading"> {data.original_title} </h1>
-          <p> {data.overview} </p>
+          <h1 className="heading"> {movieData.original_title} </h1>
+          <p> {movieData.overview} </p>
           <p>
             Genere:{" "}
-            {data.genres.map((g) => (
+            {movieData.genres.map((g) => (
               <span key={g.name}> {g.name} </span>
             ))}{" "}
           </p>
         </div>
       </div>
 
-      <Cast data={creditsData} />
+      <Cast data={castData} />
 
       {/* "63f011fc52497800dc42bac5" */}
       {/* 8hP9D6kZseM" */}
       <h2>Trailer</h2>
+      {console.log(trailerData.key)}
       <iframe
         width="560"
         height="315"
-        src={`https://www.youtube.com/embed/${trailerData.key}`}
+        src={`https://www.youtube.com/embed/${trailerKey}`}
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
+      <Link to={trailerKey}> Play Trailer </Link>
     </div>
   );
 };
--- client/src/hooks/useMovie.js
+++ client/src/hooks/useMovie.js
@@ -2,13 +2,24 @@ import { apiKey } from "../config/lib";
 import { useQuery } from "@tanstack/react-query";
 
 const getMovie = async (movieId) => {
+  const castUrl = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`;
   const movieUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`;
+  const trailerUrl = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`;
+  const urls = [movieUrl, castUrl, trailerUrl];
+
+  const allData = await Promise.all(
+    urls.map(async (url) => {
+      const res = await fetch(url);
+      return await res.json();
+    })
+  );
+
+  console.log(allData);
   const res = await fetch(movieUrl);
   if (!res.ok) {
     throw new Error("something went wrong");
   }
-  const data = await res.json();
-  return data;
+  return allData;
 };
 
 const useMovie = (id) => {
