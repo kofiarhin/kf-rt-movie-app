bf26bc52e72214118b525294feb062eadc50cbeb
kofi arhin <kofiarhin69@gmail.com>
2025-06-06 12:00:07 +0100

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000,
+    historyApiFallback: true,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
