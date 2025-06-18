b9fb79e1d29757eb3e48ad2337525c63828675c6
kofi arhin <kofiarhin69@gmail.com>
2025-06-18 10:45:20 +0100

    testing: added auth test init

--- server/__test__/auth.test.js
+++ server/__test__/auth.test.js
@@ -1,3 +1,71 @@
-describe("test", () => {
-  it("just a psssing test", () => console.log("passoign test"));
+const users = require("./data/users");
+const User = require("../models/userModel");
+const app = require("../app");
+const request = require("supertest");
+
+describe("auth", () => {
+  it("should get list of users", async () => {
+    const result = await User.find();
+    expect(result.length).toBeGreaterThan(0);
+  });
+
+  it("should login user properly", async () => {
+    const user = users[0];
+    const { body, statusCode } = await request(app)
+      .post("/api/auth/login")
+      .send({
+        email: user.email,
+        password: user.password,
+      });
+    expect(statusCode).toBe(200);
+    expect(body.token).toBeTruthy();
+  });
+
+  it("should not login user with invalid credentials", async () => {
+    const { body, statusCode } = await request(app)
+      .post("/api/auth/login")
+      .send({
+        email: users[0].email,
+        password: "invalid password",
+      });
+
+    expect(body.error).toBeDefined();
+  });
+
+  it("should register user properly", async () => {
+    const user = {
+      name: "test",
+      email: "test@gmail.com",
+      password: "password",
+    };
+    const { body, statusCode } = await request(app)
+      .post("/api/auth/register")
+      .send(user);
+    expect(body).toEqual(
+      expect.objectContaining({
+        _id: expect.any(String),
+      })
+    );
+  });
+
+  it("should not register users with missing fields", async () => {
+    const user = {
+      name: "",
+      email: "test2@gmail.com",
+      password: "password",
+    };
+
+    const { body, statusCode } = await request(app)
+      .post("/api/auth/register")
+      .send(user);
+    expect(body).toHaveProperty("error");
+  });
+
+  it("should not register duplicate users", async () => {
+    const user = users[0];
+    const { body, statusCode } = await request(app)
+      .post("/api/auth/register")
+      .send(user);
+    expect(body).toHaveProperty("error");
+  });
 });
